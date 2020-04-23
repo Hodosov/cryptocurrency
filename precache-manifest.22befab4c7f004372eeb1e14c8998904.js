67695f200e165e6608aa2aa85081a9cb12669d38
Hodosov <Hodosov.y@mail.ru>
2020-04-23 17:00:32 +0300

    Updates

---
+++ precache-manifest.22befab4c7f004372eeb1e14c8998904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9b58ea3b1ff000fd020f2b9a2e4b97",
+    "url": "/cryptocurrency/index.html"
+  },
+  {
+    "revision": "9ec126f86ff56cf6c37f",
+    "url": "/cryptocurrency/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3b3b7e6f96fcf4eaa2fe",
+    "url": "/cryptocurrency/static/js/2.83c02e28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cryptocurrency/static/js/2.83c02e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec126f86ff56cf6c37f",
+    "url": "/cryptocurrency/static/js/main.cac0d4bc.chunk.js"
+  },
+  {
+    "revision": "7f1b7ca390e7bf5bbc30",
+    "url": "/cryptocurrency/static/js/runtime-main.e8180db9.js"
+  }
+]);
